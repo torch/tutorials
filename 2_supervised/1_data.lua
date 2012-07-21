b9502963a77f811f161c9376b3721e1e7f7b6164
Clement Farabet <clement.farabet@gmail.com>
2012-07-21 12:41:38 -0400

    Corrected normalization.

--- 2_supervised/1_data.lua
+++ 2_supervised/1_data.lua
@@ -196,18 +196,20 @@ end
 print '==> preprocessing data: normalize Y (luminance) channel locally'
 
 -- Define the normalization neighborhood:
-neighborhood = image.gaussian1D(7)
+neighborhood = image.gaussian1D(13)
 
 -- Define our local normalization operator (It is an actual nn module, 
 -- which could be inserted into a trainable model):
-normalization = nn.SpatialContrastiveNormalization(1, neighborhood):float()
+normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1e-3):float()
 
--- Normalize all Y channels locally:
-for i = 1,trainData:size() do
-   trainData.data[{ i,{1},{},{} }] = normalization:forward(trainData.data[{ i,{1},{},{} }])
-end
-for i = 1,testData:size() do
-   testData.data[{ i,{1},{},{} }] = normalization:forward(testData.data[{ i,{1},{},{} }])
+-- Normalize all channels locally:
+for c in ipairs(channels) do
+   for i = 1,trainData:size() do
+      trainData.data[{ i,{c},{},{} }] = normalization:forward(trainData.data[{ i,{c},{},{} }])
+   end
+   for i = 1,testData:size() do
+      testData.data[{ i,{c},{},{} }] = normalization:forward(testData.data[{ i,{c},{},{} }])
+   end
 end
 
 ----------------------------------------------------------------------
