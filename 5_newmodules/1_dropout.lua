4f03e96d014e889d9cb25e9f3da041e38b455e47
Clement Farabet <clement.farabet@gmail.com>
2014-02-05 00:50:11 -0500

    oops

--- 5_newmodules/1_dropout.lua
+++ 5_newmodules/1_dropout.lua
@@ -3,19 +3,20 @@
 require 'nn'
 require 'Dropout'
 require 'image'
+require 'gfx.js'
 
 -- define a dropout object:
 n = nn.Dropout(0.5)
 
 -- load an image:
-i = image.lena(true)
+i = image.lena()
 
 -- process the image:
 result = n:forward(i)
 
 -- display results:
-image.display{image=i, legend='original image'}
-image.display{image=result, legend='dropout-processed image'}
+gfx.image(i, {legend='original image'})
+gfx.image(result, {legend='dropout-processed image'})
 
 -- some stats:
 mse = i:dist(result)
