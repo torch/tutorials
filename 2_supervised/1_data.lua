1af967b17a6cdf12e52bd7fa551191925404b713
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-07-01 15:10:15 +0530

    Change executable name for torch.
    s/torch/th in 2_supervised

--- 2_supervised/1_data.lua
+++ 2_supervised/1_data.lua
@@ -7,7 +7,7 @@
 -- to the quite known MNIST challenge.
 --
 -- It's a good idea to run this script with the interactive mode:
--- $ torch -i 1_data.lua
+-- $ th -i 1_data.lua
 -- this will give you a Torch interpreter at the end, that you
 -- can use to analyze/visualize the data you've just loaded.
 --
